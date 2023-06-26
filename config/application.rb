7dfef016b6b440824870d0a514fa54057ca06d5d
fascilio <fascilior@gmail.com>
2023-06-26 23:34:30 +0300

    active record associations

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,10 @@ module Phase4ActiveRecordAssociationsReviewReadme
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # config.time_zone = ActiveSupport::TimeZone['EAT']
+    # config.time_zone = 'EAT'
+    # config.time_zone = 'Nairobi'
+    # config.active_record.default_timezone = :local
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
